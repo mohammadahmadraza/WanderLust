95d9268effe4b610c796be4fe47e933fb7f9fb4b
Muhammad Ahmad Raza <muhammadahmadraza105@gmail.com>
2024-11-17 12:23:00 +0500

    Create schema validation using JOI for listing for server side validation

---
+++ schema.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        imageURL: Joi.string().allow("", null),
+        price: Joi.number().min(0),
+        location: Joi.string(),
+        country: Joi.string().required()
+    }).required()
+});
+
