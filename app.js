2ca9701baa1f1a8acfedaad495816a2d9a2351a6
Muhammad Ahmad Raza <muhammadahmadraza105@gmail.com>
2024-11-18 22:37:04 +0500

    Create all routes for listings separately

--- app.js
+++ app.js
@@ -24,24 +24,6 @@ app.use(methodoverride('_method'));
 app.engine('ejs', ejsMate);
 
 
-
-// Middleware function for schema validation
-
-function listingValidation(req, res, next) {
-
-    let { error } = listingSchema.validate(req.body);
-    console.log('Listing Validation Error', error);
-    if (error) {
-        throw new ExpressError(400, 'Please send valid listing data.');
-    }
-    else {
-        next();
-
-    }
-
-}
-
-
 // function to connect with database and function call
 const main = async () => {
     await mongoose.connect(MONGOOSEURL);
@@ -53,77 +35,6 @@ main().then(res => console.log('Connected to database successfully.'))
 
 app.use('/listings', listingRoutes);
 
-// Show all listing route
-app.get('/listings', async (req, res) => {
-    const listings = await Listing.find();
-    res.render('listing/show.ejs', { listings });
-});
-
-app.get('/', WrapAsync(async (req, res) => {
-    const listings = await Listing.find();
-    res.render('listing/show.ejs', { listings });
-}));
-
-
-// Form to add new listing
-// app.get('/listings/addnew', (req, res) => {
-
-//     res.render('listing/add.ejs');
-// });
-
-// Save new listing data to database
-app.post('/listings/addnew', listingValidation, WrapAsync(async (req, res) => {
-    let { title, description, imageURL, price, location, country } = req.body;
-
-    const newListing = new Listing({
-        title: title,
-        description: description,
-        imageURL: imageURL,
-        price: price ? price : 0,
-        location: location,
-        country: country
-    });
-    await newListing.save();
-    res.redirect('/listings');
-}));
-
-// Form to edit listing
-// app.get('/listings/:listing_id/edit', WrapAsync(async (req, res) => {
-//     const listing = await Listing.findById(req.params.listing_id);
-//     res.render('listing/edit.ejs', { listing });
-// }));
-
-// Save changes in edit listing
-app.put('/listings/:listing_id', listingValidation, WrapAsync(async (req, res) => {
-    const { title, description, imageURL, price, location, country } = req.body;
-    const updated_listing = {
-        title: title,
-        description: description,
-        imageURL: imageURL,
-        price: price ? price.replace(/,/g, '') : 0,
-        location: location,
-        country: country
-    }
-    const listing = await Listing.findByIdAndUpdate(req.params.listing_id, updated_listing, { runValidators: true });
-    res.redirect('/listings');
-}));
-
-// View details of listing
-app.get('/listings/:listing_id/view', WrapAsync(async (req, res) => {
-    const listing = await Listing.findById(req.params.listing_id).populate('reviews');
-    res.render('listing/view.ejs', { listing });
-}));
-
-// Delete listing from database
-app.delete('/listings/:listing_id', WrapAsync(async (req, res) => {
-
-    await Listing.findByIdAndDelete(req.params.listing_id)
-        .then(() => res.redirect('/listings'))
-        .catch((err) => {
-            res.send('Cannot delete due to error', err);
-        })
-
-}));
 
 
 // Reviews End points
--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -1,12 +1,92 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
+const Listing = require('../models/Listing');
+const WrapAsync = require('../utilis/WrapAsync');
+const ExpressError = require('../utilis/ExpressError');
+const {listingSchema} = require('../')
 
+
+// Middleware function for schema validation
+
+function listingValidation(req, res, next) {
+
+    let { error } = listingSchema.validate(req.body);
+    console.log('Listing Validation Error', error);
+    if (error) {
+        throw new ExpressError(400, 'Please send valid listing data.');
+    }
+    else {
+        next();
+
+    }
+
+}
+
+// Show all listing route
+router.get('/', async (req, res) => {
+    const listings = await Listing.find();
+    res.render('listing/show.ejs', { listings });
+});
 
 // Form to add new listing
 router.get('/addnew', (req, res) => {
-// res.send('Form to add new listing');
     res.render('listing/add.ejs');
 });
+
+// Save new listing data to database
+router.post('/addnew', listingValidation, WrapAsync(async (req, res) => {
+    let { title, description, imageURL, price, location, country } = req.body;
+
+    const newListing = new Listing({
+        title: title,
+        description: description,
+        imageURL: imageURL,
+        price: price ? price : 0,
+        location: location,
+        country: country
+    });
+    await newListing.save();
+    res.redirect('/');
+}));
+
+// Form to edit listing
+router.get('/:listing_id/edit', WrapAsync(async (req, res) => {
+    const listing = await Listing.findById(req.params.listing_id);
+    res.render('listing/edit.ejs', { listing });
+}));
+
+// Save changes in edit listing
+router.put('/:listing_id', listingValidation, WrapAsync(async (req, res) => {
+    const { title, description, imageURL, price, location, country } = req.body;
+    const updated_listing = {
+        title: title,
+        description: description,
+        imageURL: imageURL,
+        price: price ? price.replace(/,/g, '') : 0,
+        location: location,
+        country: country
+    }
+    const listing = await Listing.findByIdAndUpdate(req.params.listing_id, updated_listing, { runValidators: true });
+    res.redirect('/');
+}));
+
+// View details of listing
+router.get('/:listing_id/view', WrapAsync(async (req, res) => {
+    const listing = await Listing.findById(req.params.listing_id).populate('reviews');
+    res.render('listing/view.ejs', { listing });
+}));
+
+// Delete listing from database
+router.delete('/:listing_id', WrapAsync(async (req, res) => {
+
+    await Listing.findByIdAndDelete(req.params.listing_id)
+        .then(() => res.redirect('/listings'))
+        .catch((err) => {
+            res.send('Cannot delete due to error', err);
+        })
+
+}));
+
 
 
 module.exports = router;
